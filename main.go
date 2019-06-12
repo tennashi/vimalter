16d6e160b11da77f45c1204ffb7edfcf070e9cb2
Yuya OTA <yuya.gt@gmail.com>
2019-06-12 23:30:38 +0900

    refactor main func and use tennashi/tapiexec

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 
 	"github.com/shirou/gopsutil/process"
+	"github.com/tennashi/tapiexec"
 )
 
 var (
@@ -20,64 +20,113 @@ func main() {
 	flag.Parse()
 	args := flag.Args()
 
-	var vim *exec.Cmd
 	vimRuntime := os.Getenv("VIMRUNTIME")
 	if vimRuntime == "" {
-		vim = exec.Command("vim", args...)
+		err := runVim(args)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		os.Exit(0)
 	}
 
 	vimEnv := filepath.Dir(vimRuntime)
 	switch filepath.Base(vimEnv) {
 	case "vim":
 		srvName := os.Getenv("VIM_SERVERNAME")
+
 		if srvName == "" {
-			if len(args) == 0 {
-				fmt.Fprintln(os.Stderr, "open new file is not supported in vim8(server not started)")
+			err := runVimTapi(args)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
 				os.Exit(1)
 			}
-			if *tabFlg {
-				fmt.Fprintln(os.Stderr, "open the file in new tab is not supported in vim8(server not started)")
+		} else {
+			err := runVimCS(srvName, args)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
 				os.Exit(1)
-			} else {
-				vim = exec.Command("echo", "-e", "\x1b]51;[\"drop\",\""+strings.Join(args, " ")+"\"]\x07")
 			}
-			break
 		}
-		path, err := evalVimPath()
+
+		os.Exit(0)
+
+	case "nvim":
+		err := runNvimNVR(args)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		if len(args) == 0 {
-			if *tabFlg {
-				args = []string{"--servername", srvName, "--remote-send", `<C-\><C-N>:tabnew<CR>`}
-			} else {
-				args = []string{"--servername", srvName, "--remote-send", `<C-\><C-N>:new<CR>`}
-			}
-		} else {
-			if *tabFlg {
-				args = append([]string{"--servername", srvName, "--remote-tab"}, args...)
-			} else {
-				args = append([]string{"--servername", srvName, "--remote"}, args...)
-			}
-
-		}
-		vim = exec.Command(path, args...)
-
-	case "nvim":
-		if *tabFlg {
-			args = append([]string{"--remote-tab-wait"}, args...)
-		} else {
-			args = append([]string{"--remote-wait"}, args...)
-		}
-		vim = exec.Command("nvr", args...)
+		os.Exit(0)
 	}
+}
+
+func runVim(args []string) error {
+	vim := exec.Command("vim", args...)
 
 	vim.Stdin = os.Stdin
 	vim.Stdout = os.Stdout
 	vim.Stderr = os.Stderr
 
-	vim.Run()
+	return vim.Run()
+}
+
+func runVimTapi(args []string) error {
+	if *tabFlg {
+		args = append([]string{"tabnew"}, args...)
+	} else {
+		args = append([]string{"split"}, args...)
+	}
+	tapi := tapiexec.CallAPI("Tapi_open_wait", args)
+
+	err := tapi.Run()
+	if err != nil {
+		return err
+	}
+	return tapiexec.WaitMsg("done")
+}
+
+func runVimCS(srvName string, args []string) error {
+	path, err := evalVimPath()
+	if err != nil {
+		return err
+	}
+
+	if len(args) == 0 {
+		if *tabFlg {
+			args = []string{"--servername", srvName, "--remote-send", `<C-\><C-N>:tabnew<CR>`}
+		} else {
+			args = []string{"--servername", srvName, "--remote-send", `<C-\><C-N>:new<CR>`}
+		}
+	} else {
+		if *tabFlg {
+			args = append([]string{"--servername", srvName, "--remote-tab"}, args...)
+		} else {
+			args = append([]string{"--servername", srvName, "--remote"}, args...)
+		}
+
+	}
+	vim := exec.Command(path, args...)
+	vim.Stdin = os.Stdin
+	vim.Stdout = os.Stdout
+	vim.Stderr = os.Stderr
+
+	return vim.Run()
+}
+
+func runNvimNVR(args []string) error {
+	if *tabFlg {
+		args = append([]string{"--remote-tab-wait"}, args...)
+	} else {
+		args = append([]string{"--remote-wait"}, args...)
+	}
+	vim := exec.Command("nvr", args...)
+
+	vim.Stdin = os.Stdin
+	vim.Stdout = os.Stdout
+	vim.Stderr = os.Stderr
+
+	return vim.Run()
 }
 
 func evalVimPath() (string, error) {
